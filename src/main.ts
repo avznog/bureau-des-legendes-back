7736ec80f5b3c944660a82e80fcb426f168f94a7
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-07-24 01:41:19 +0200

    🦺 add socket url to cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.BASE_URL || 'http://localhost:3000',
+    origin: [process.env.BASE_URL || 'http://localhost:3000', process.env.SOCKET_URL],
   });
   await app.listen(8000);
 }
